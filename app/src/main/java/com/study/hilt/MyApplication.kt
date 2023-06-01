d3048d0ce8891fc5f58e1caff1d12c47f740b8f5
iamso0 <iamsoyoun9@naver.com>
2023-06-01 14:57:08 +0900

    start

--- app/src/main/java/com/study/hilt/MyApplication.kt
+++ app/src/main/java/com/study/hilt/MyApplication.kt
@@ -1,29 +1,2 @@
 package com.study.hilt
 
-import android.app.Application
-import android.content.Context
-import androidx.lifecycle.LifecycleObserver
-import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
-
-/**
- * @HiltAndroidApp : Hilt 컴포넌트의 코드 생성과 컴포넌트를 사용하는 Application의 기본 클래스를 생성
- **/
-@HiltAndroidApp
-class MyApplication : Application(), LifecycleObserver {
-
-    companion object {
-        private const val TAG = "MyApplication"
-        private var instance: MyApplication? = null
-        val applicationContext: Context get() = instance!!.applicationContext
-        var isAppOnForeground = false
-    }
-
-    init {
-        instance = this
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
